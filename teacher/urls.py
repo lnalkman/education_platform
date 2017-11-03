78b44a9df192c3f20994a6fc893419708d3c9023
Oleksandr <lnalkman@gmail.com>
2017-11-03 21:45:47 +0200

    Доробив генерацію календаря та підправив розмітку/стилі
    Signed-off-by: Oleksandr <lnalkman@gmail.com>

--- teacher/urls.py
+++ teacher/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import url
 
 from .views import (
     TeacherProfile, AddCourseView, CourseListView,
-    CourseDetail, CalendarView
+    CourseDetail, CalendarView, CalendarRedirect
 )
 
 
@@ -11,5 +11,6 @@ urlpatterns = [
     url(r'^course/add/$', AddCourseView.as_view(), name='course-add'),
     url(r'^courses/$', CourseListView.as_view(), name='course-list'),
     url(r'^course/(?P<pk>[0-9]+)/$', CourseDetail.as_view(), name='course-detail'),
+    url(r'^calendar/$', CalendarRedirect.as_view(), name='calendar-redirect'),
     url(r'^calendar/(?P<year>[0-9]+)-(?P<month>[0-9]+)/$', CalendarView.as_view(), name='calendar'),
 ]
--- teacher/views.py
+++ teacher/views.py
@@ -1,4 +1,5 @@
 from calendar import HTMLCalendar
+from calendar import month_name
 from datetime import datetime
 from functools import partial
 
@@ -7,8 +8,8 @@ from django.shortcuts import reverse
 from django.views.generic.edit import FormView, UpdateView
 from django.views.generic.list import ListView
 from django.contrib.auth.mixins import UserPassesTestMixin, LoginRequiredMixin
-from django.views.generic.base import TemplateView
-
+from django.views.generic.base import TemplateView, RedirectView
+from django.http.response import HttpResponseRedirect
 
 from .models import Course, CalendarNote
 from .forms import CourseForm, TeacherProfileForm
@@ -79,19 +80,34 @@ class CourseDetail(TeacherRequiredMixin, UpdateView):
 
 
 class HrefCalendar(HTMLCalendar):
-
     # Якщо в цей день є якісь події/нотатки
     booked_class = 'booked'
 
     href = '#'
 
-    def __init__(self, theyear, themonth, booked_days=None):
+    def __init__(self, theyear, themonth, prew_url='#', next_url='#', booked_days=None):
         """:booked_days -> контейнер в якому зберігаються дні в місяці в яких є якісь нотатки"""
         HTMLCalendar.__init__(self)
         self.theyear = theyear
         self.themonth = themonth
+        self.prew_url = prew_url
+        self.next_url = next_url
         self.booked_days = booked_days
         # self.href = partial(reverse, year=theyear, month=)
+
+    def formatmonthname(self, theyear, themonth, withyear=True):
+        """
+        Return a month name as a table row.
+        """
+        if withyear:
+            s = '%s %s' % (month_name[themonth], theyear)
+        else:
+            s = '%s' % month_name[themonth]
+        return '<tr><th colspan="7" class="month">' \
+               '<a href="%s" class="glyphicon glyphicon-chevron-left"></a>' \
+               '%s' \
+               '<a href="%s" class="glyphicon glyphicon-chevron-right"></a>' \
+               '</th></tr>' % (self.prew_url, s, self.next_url)
 
     def formatday(self, day, weekday, booked=False):
 
@@ -111,37 +127,77 @@ class HrefCalendar(HTMLCalendar):
         s = ''.join(self.formatday(d, wd, d in self.booked_days) for (d, wd) in theweek)
         return '<tr>%s</tr>' % s
 
-    def formatmonth(self, withyear=True):
-        """
-        Return a formatted month as a table.
-        """
-        v = []
-        a = v.append
-        a('<table border="0" cellpadding="0" cellspacing="0" class="month">')
-        a('\n')
-        a(self.formatmonthname(self.theyear, self.themonth, withyear=withyear))
-        a('\n')
-        a(self.formatweekheader())
-        a('\n')
-        for week in self.monthdays2calendar(self.theyear, self.themonth):
-            a(self.formatweek(week))
-            a('\n')
-        a('</table>')
-        a('\n')
-        return ''.join(v)
+    def formatmonth(self, theyear=None, themonth=None, withyear=False):
+        return HTMLCalendar.formatmonth(
+            self,
+            theyear or self.theyear,
+            themonth or self.themonth,
+            withyear
+        )
 
 
-class CalendarView(TemplateView):
+class CalendarView(TeacherRequiredMixin, TemplateView):
+    http_method_names = ('get',)
     template_name = 'teacher/calendar.html'
+
+    def get(self, *args, **kwargs):
+        try:
+            year, month = int(kwargs.get('year', 0)), int(kwargs.get('month', 0))
+            if not (isinstance(year, int) and isinstance(month, int)
+                    and 1995 < year < 2500 and 0 < month <= 12):
+                raise ValueError('Invalid year or month value.')
+        except ValueError:
+            return HttpResponseRedirect(reverse('calendar-redirect'))
+        return TemplateView.get(self, *args, **kwargs)
+
+    def get_booked_days(self, year, month):
+        """Повертає множину днів заданого року та місяця у яких є події цього викладача"""
+        return {q.date.day for q in CalendarNote.objects.filter(
+            author=self.request.user.profile,
+            date__year=year,
+            date__month=month
+        )}
+
+    def get_prev_month_url(self, year, month):
+        """Повертає посилання не попередній місяць"""
+        if month == 1:
+            return reverse('teacher:calendar', kwargs={'year': year - 1, 'month': 12})
+        return reverse('teacher:calendar', kwargs={'year': year, 'month': month - 1})
+
+    def get_next_month_url(self, year, month):
+        """Повертає посилання не наступний місяць"""
+        if month == 12:
+            return reverse('teacher:calendar', kwargs={'year': year + 1, 'month': 1})
+        return reverse('teacher:calendar', kwargs={'year': year, 'month': month + 1})
 
     def get_context_data(self, **kwargs):
         context = super(TemplateView, self).get_context_data(**kwargs)
         context['curr_year'], context['curr_month'] = datetime.now().timetuple()[:2]
-        booked_days = [
-            day for day in CalendarNote.objects.filter(date__year=int(kwargs['year']), date__month=int(kwargs['month']))
-        ]
-        context['calendar_table'] = HrefCalendar(theyear=int(kwargs['year']),
-                                                 themonth=int(kwargs['month']),
-                                                 booked_days=booked_days,
-                                                 ).formatmonth()
+        year = int(kwargs['year'])
+        month = int(kwargs['month'])
+        context['year'], context['month'] = year, month
+        context['prev_month_url'] = self.get_prev_month_url(year, month)
+        context['next_month_url'] = self.get_next_month_url(year, month)
+
+        booked_days = self.get_booked_days(year, month)
+        context['calendar_table'] = HrefCalendar(
+            theyear=year,
+            themonth=month,
+            prew_url=self.get_prev_month_url(year, month),
+            next_url=self.get_next_month_url(year, month),
+            booked_days=booked_days,
+        ).formatmonth()
+
         return context
+
+
+class CalendarRedirect(RedirectView):
+    def get_redirect_url(self, *args, **kwargs):
+        if self.request.GET:
+            year, month = self.request.GET.get('year', 0), self.request.GET.get('month', 0)
+            if not (isinstance(year, int) and isinstance(month, int)
+                and 1995 < year < 2500 and 0 < month <= 12):
+                year, month = datetime.now().timetuple()[:2]
+        else:
+            year, month = datetime.now().timetuple()[:2]
+        return reverse('teacher:calendar', kwargs={'year': year, 'month': month})
