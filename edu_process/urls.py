c59caa1e6095807cde06fdbcd230025aa2801ca0
Oleksandr <lnalkman@gmail.com>
2017-11-26 17:49:10 +0200

    Додав пошук груп за назвою.

--- edu_process/urls.py
+++ edu_process/urls.py
@@ -1,10 +1,11 @@
 from django.conf.urls import url
 from django.views.generic.base import TemplateView
 from .views import (
-    IndexView,
+    IndexView, SearchGroup
 )
 
 urlpatterns = [
     url(r'^$', IndexView.as_view(), name='index'),
+    url(r'^group/search/$', SearchGroup.as_view(), name='search-group'),
     url(r'^profile/student/$', TemplateView.as_view(template_name='teacher/teacher-account.html'), name='student-profile'),
 ]
--- edu_process/views.py
+++ edu_process/views.py
@@ -1,11 +1,13 @@
+from django.core import serializers
 from django.contrib.auth import login
 from django.shortcuts import reverse
 from django.contrib.auth.forms import AuthenticationForm
+from django.views.generic.base import View
 from django.views.generic.edit import FormView
-from django.http.response import HttpResponseRedirect
+from django.http.response import HttpResponseRedirect, HttpResponse
 from django.urls import reverse_lazy
 
-from .models import Profile
+from .models import Profile, Group
 
 
 class IndexView(FormView):
@@ -40,3 +42,36 @@ class IndexView(FormView):
         elif user_type == Profile.STUDENT:
             return reverse('student-profile')
 
+
+class SearchGroup(View):
+    http_method_names = ('get',)
+    model = Group
+    # Стандартна кількість записів, які повертає view
+    returned_object_count = 5
+    # Максимальна кількість записів, яку може повернути view
+    limit_returned_objects = 15
+
+    def render_to_json(self, queryset=None):
+        json = serializers.serialize('json', queryset, fields=('name',))
+        return HttpResponse(json, content_type='application/json')
+
+    def get_objects(self, q, val=None):
+        queryset = self.model.objects.filter(name__icontains=q)
+        if val:
+            return queryset[:val]
+        return queryset
+
+    def get(self, request, *args, **kwargs):
+        # Строка пошуку
+        q = request.GET.get('q')
+
+        # Кількість резульатів, які протрібно повернути
+        try:
+            val = int(request.GET.get('val', -1))
+            if val not in range(1, self.limit_returned_objects + 1):
+                raise ValueError
+        except ValueError:
+            val = self.returned_object_count
+
+        queryset = self.get_objects(q, val)
+        return self.render_to_json(queryset)
