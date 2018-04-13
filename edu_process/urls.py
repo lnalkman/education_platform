83229ff14203f3b962245f371eb2227e8e454d4d
alkman <lnalkman@gmail.com>
2018-04-13 20:05:03 +0300

    Додав шаблон сторінку перегляду деталей курсу.

--- edu_process/urls.py
+++ edu_process/urls.py
@@ -12,6 +12,7 @@ urlpatterns = [
     url(r'^api/messages/list/$', MessagesApiView.as_view()),
     url(r'^api/messages/users/$', MessageUserApiView.as_view()),
     url(r'^avatar/$', UserAvatarView.as_view(), name='user-avatar'),
-    url(r'^auth/logout/$', LogoutView.as_view(), name='logout')
+    url(r'^auth/logout/$', LogoutView.as_view(), name='logout'),
+    url(r'^course/id/$', TemplateView.as_view(template_name='edu_process/course-detail.html'), name='course-detail'),
 
 ]
